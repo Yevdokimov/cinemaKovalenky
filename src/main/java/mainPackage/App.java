15e12d25922b351fad2aa98b04044b5a0ec3a340
yevdo <yevdokimov.denis1@gmail.com>
2023-02-23 21:20:53 +0200

    Коміт в якому працює метод криейт, але помилково записує у ключ весь масив. Також можна повернути через рід обєект фільму, але працює не через ключ

--- src/main/java/mainPackage/App.java
+++ src/main/java/mainPackage/App.java
@@ -32,8 +32,6 @@ public class App {
         System.out.println(key[3]);
         System.out.println(filmGenericStorageApp.read(key[4]));
         System.out.println(filmGenericStorageApp.read(key[2]));
-        System.out.println(filmGenericStorageApp.read(key[2]));
-
 
     }
 }
